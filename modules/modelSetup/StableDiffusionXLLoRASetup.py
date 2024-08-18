ddf8185bc696ef4c0fc0a1e13523f94194caeb7f
sayat <sayt9360@gmail.com>
2024-08-18 23:20:14 +0900

    lora text encoder fix

--- modules/modelSetup/StableDiffusionXLLoRASetup.py
+++ modules/modelSetup/StableDiffusionXLLoRASetup.py
@@ -136,11 +136,11 @@ class StableDiffusionXLLoRASetup(
         create_te2 = config.text_encoder_2.train or state_dict_has_prefix(model.lora_state_dict, "lora_te2")
 
         model.text_encoder_1_lora = LoRAModuleWrapper(
-            model.text_encoder_1, "lora_te1", config
+            model.text_encoder_1, "lora_te1", config, ["text_model"]
         ) if create_te1 else None
 
         model.text_encoder_2_lora = LoRAModuleWrapper(
-            model.text_encoder_2, "lora_te2", config
+            model.text_encoder_2, "lora_te2", config, ["text_model"]
         ) if create_te2 else None
 
         model.unet_lora = LoRAModuleWrapper(
