afac6eac871b948d4bc0d295a3d6812a831811a6
sayat <sayt9360@gmail.com>
2024-09-19 17:39:41 +0900

    1

--- modules/util/config/TrainConfig.py
+++ modules/util/config/TrainConfig.py
@@ -140,7 +140,7 @@ class TrainOptimizerConfig(BaseConfig):
         data.append(("relative_step", None, bool, False))
         data.append(("safeguard_warmup", None, bool, False))
         data.append(("scale_parameter", None, bool, False))
-        data.append(("stochastic_rounding", True, bool, False))
+        data.append(("stochastic_rounding", None, bool, False))
         data.append(("use_bias_correction", None, bool, False))
         data.append(("use_triton", None, bool, False))
         data.append(("warmup_init", None, bool, False))
@@ -157,7 +157,7 @@ class TrainOptimizerConfig(BaseConfig):
         data.append(("ams_bound", None, bool, False))
         data.append(("r", None, float, True))
         data.append(("adanorm", None, bool, False))
-        data.append(("adam_debias", False, bool, False))
+        data.append(("adam_debias", None, bool, False))
 
         return TrainOptimizerConfig(data)
 
